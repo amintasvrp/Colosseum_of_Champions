38fed752c39824a43f840a7f4dc1baf722dae051
Andre Matos <andre.matos@ccc.ufcg.edu.br>
2018-05-18 15:11:09 -0300

    Criada base das funcoes de interacao

--- Front/begin.cpp
+++ Front/begin.cpp
@@ -1,12 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <iostream>
+#include <string>
 
+
+
+
+
+    char venceuBatalha() {
+        printf("Muito bem, voce avancou ao proximo nivel!!\n");
+        printf("Ainda faltam" + jogadasQueFaltam + "batalhas");
+
+        //recebeHeroi()
+    }
+
+    char perdeuBatalha() {
+        printf("Game Over, mais sorte na proxima tentativa, se tiver coragem...");
+    }
+
+    char venceuJogo() {
+        printf("Parabens, a voce pertence a gloria dos campeoes!!!");
+    }
 
 int main(){
-    printf("Bem vindo ao Colosseum of Champions!! \n" );
+    printf("Welcome to the Colosseum of Champions!! \n" );
     printf("Preparesse para uma grande aventura que pode resultar na sua gloria...\n");
-    printf("...ou no seu esquecimento...");
+    printf("...ou no seu esquecimento...\n");
     
+
+    int jogadasQueFaltam = 10; 
+    int x;
+    x = 5;
+    int y;
+    y = 0;
+
+
+    /* 
+     * Printar lista com os herois do jogador com o seu index + 1
+    */
+
+
+
+    if(y == 10) {
+        perdeuBatalha();
+    }
+    
+    // if(vitoriaBatalha) {
+    //    venceuBatalha(); 
+    // }
+    /*if(vitoriaFinal) {
+
+    }*/
  return 0;
 }
