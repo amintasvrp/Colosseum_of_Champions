a3f22ee2f99a5e5857876950161617a81aff150a
Andre Matos <andre.matos@ccc.ufcg.edu.br>
2018-05-18 14:18:07 -0300

    Primeira mensagem

---
+++ Front/begin.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(){
+    printf("Bem vindo ao Colosseum of Champions!! \n" );
+    printf("Preparesse para uma grande aventura que pode resultar na sua gloria...\n");
+    printf("...ou no seu esquecimento...");
+    
+ return 0;
+}
