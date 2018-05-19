7634b7e06c9bc90c4eabf8132b2e44bf060d0913
ItamarFarias <itamar.farias@ccc.ufcg.edu.br>
2018-05-19 11:15:14 -0300

    Update begin.cpp

--- Front/begin.cpp
+++ Front/begin.cpp
@@ -3,10 +3,11 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
 
     char battleWon() {
-        std::cout << "Very good, you advanced to the next level!!!\n" << std::endl;
-        std::cout << "Are still missing "<< jogadasQueFaltam << " battles" << std::endl;
+        cout << "Very good, you advanced to the next level!!!\n" << endl;
+        cout << "Are still missing "<< jogadasQueFaltam << " battles" << endl;
 
         //recebeHeroi()
     }
@@ -24,18 +25,28 @@
     cout << "\$$$$$$  |$$ |  $$ |$$ | \_/ $$ |$$$$$$$$\        $$$$$$  |   \$  /   $$$$$$$$\ $$ |  $$ |" << endl;
     cout << " \______/ \__|  \__|\__|     \__|\________|       \______/     \_/    \________|\__|  \__|" << endl;
 
-    cout << "\nmore luck in the next attempt, if you have the courage..." << endl;
+    cout << "\nMore luck in the next attempt, if you have the courage..." << endl;
 
 }
 
-    char gameWon() {
-        std::cout << "Congratulations, you belong to the glory of the champions!!!" << std::endl;
+    gameWon() {
+
+        cout << "   ___                            _         _       _   _" << endl;
+        cout << "  / __\___  _ __   __ _ _ __ __ _| |_ _   _| | __ _| |_(_) ___  _ __  ___" << endl;
+        cout << " / /  / _ \| '_ \ / _` | '__/ _` | __| | | | |/ _` | __| |/ _ \| '_ \/ __|" << endl;
+        cout << "/ /__| (_) | | | | (_| | | | (_| | |_| |_| | | (_| | |_| | (_) | | | \__ \ " << endl;
+        cout << "\____/\___/|_| |_|\__, |_|  \__,_|\__|\__,_|_|\__,_|\__|_|\___/|_| |_|___/" << endl;
+        cout << "                  |___/" << endl;
+
+        cout << "\n\nYou belong to the glory of the champions!!!" << endl;
+
     }
 
+
 int main(){
-    std::cout << "Welcome to the Colosseum of Champions!!!" << std::endl;
-    std::cout << "Get ready for a great adventure that could result in your glory..." << std::endl;
-    std::cout << "...or in its oblivion...." << std::endl;    
+    cout << "Welcome to the Colosseum of Champions!!!" << endl;
+    cout << "Get ready for a great adventure that could result in your glory..." << std::endl;
+    cout << "...or in its oblivion...." << endl;    
 
     int jogadasQueFaltam = 10; 
     int x;
